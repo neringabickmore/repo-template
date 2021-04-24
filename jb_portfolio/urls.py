1e9ef2db812c584f32dd285e3977a604da416cc4
neringa <neringa.bickmore@gmail.com>
2021-04-24 23:38:41 +0100

    portfolio-app/changed colour scheme

--- jb_portfolio/urls.py
+++ jb_portfolio/urls.py
@@ -20,8 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bio.urls')),
+    path('bio/', include('bio.urls')),
     path('contact/', include('contact.urls')),
-    path('portfolio/', include('portfolio.urls')),
+    path('', include('portfolio.urls')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
