ad4947999357bf89602aff6a038902fbc98f7a47
neringa <neringa.bickmore@gmail.com>
2021-04-26 00:15:32 +0100

    Photo-models/Added models, admin and migrated

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+class Photo (models.Model) :
+
+    """
+    Model for Photos.
+    """
+    class Meta:
+        verbose_name_plural = "Photos"
+
+    title = models.CharField("Title", max_length=200)
+    friendly_title = models.CharField("Friendly Title", max_length=200)
+    date_time = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField('image', blank=False)
+
+    sort = ('date_time')
+
+    def __str__(self):
+        return self.title
+
+    def get_friendly_name(self):
+        return self.friendly_title
