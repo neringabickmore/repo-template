182719093ebef2727d668ace070ae1d9b4b2ef91
neringa <neringa.bickmore@gmail.com>
2021-05-02 22:15:21 +0100

    js/portfolio/Updated functions

--- portfolio/static/portfolio/js/portfolio.js
+++ portfolio/static/portfolio/js/portfolio.js
@@ -1,69 +1,71 @@
+
 /**
  * Global Event listeners
  */
  $(".btnOne").click(function () {
-    $('.btnOne').addClass('active')
+    $('.btnOne').addClass('active');
+    $('.btnTwo').removeClass('active');
     photoGridOne();
 });
 
 $(".btnTwo").click(function () {
     $('.btnTwo').addClass('active')
+    $('.btnOne').removeClass('active')
+    $('.btnThree').removeClass('active')
+    $('.btnFour').removeClass('active')
     photoGridTwo();
 });
 
 $(".btnThree").click(function () {
     $('.btnThree').addClass('active')
+    $('.btnOne').removeClass('active')
+    $('.btnTwo').removeClass('active')
+    $('.btnFour').removeClass('active')
     photoGridThree();
 });
 
 $(".btnFour").click(function () {
     $('.btnFour').addClass('active')
+    $('.btnOne').removeClass('active')
+    $('.btnTwo').removeClass('active')
+    $('.btnThree').removeClass('active')
     photoGridFour();
 });
 
 
 /**
  * function to enable the view of one photo per column
+ * on small screens, else two
  */
  const photoGridOne = () => {
-    $('#photoGridRow>div.photoGrid').addClass('col-12')
+    $('#photoGridRow>div.photoGrid').addClass(['col-12', 'col-sm-6'])
     $('#photoGridRow>div.photoGrid').removeClass('col-6')
-    $('.btnTwo').removeClass('active')
 };
 
 /**
  * function to enable the view of three photos per column
+ * on screens larger than small, else two.
  */
 const photoGridThree = () => {
-    $('#photoGridRow>div.photoGrid').addClass('col-4')
-    $('#photoGridRow>div.photoGrid').removeClass('col-3')
-    $('#photoGridRow>div.photoGrid').removeClass('col-6')
-    $('.btnTwo').removeClass('active')
-    $('.btnFour').removeClass('active')
+    $('#photoGridRow>div.photoGrid').addClass(['col-sm-4', 'col-6'])
+    $('#photoGridRow>div.photoGrid').removeClass(['col-sm-3', 'col-sm-6'])
 };
 
 /**
  * function to enable the view of two photos per column
+ * on screens larger than small, else two
  */
 
 const photoGridTwo = () => {
-    $('#photoGridRow>div.photoGrid').removeClass('col-3')
-    $('#photoGridRow>div.photoGrid').removeClass('col-4')
-    $('#photoGridRow>div.photoGrid').removeClass('col-12')
-    $('#photoGridRow>div.photoGrid').addClass('col-6')
-    $('.btnOne').removeClass('active')
-    $('.btnThree').removeClass('active')
-    $('.btnFour').removeClass('active')
+    $('#photoGridRow>div.photoGrid').addClass(['col-sm-6', 'col-6'])
+    $('#photoGridRow>div.photoGrid').removeClass(['col-sm-3', 'col-sm-4', 'col-12'])
 };
 
 /**
  * function to enable the view of Four photos per column
+ * on screens larger than small, else two
  */
 const photoGridFour = () => {
-    $('#photoGridRow>div.photoGrid').removeClass('col-4')
-    $('#photoGridRow>div.photoGrid').removeClass('col-6')
-    $('#photoGridRow>div.photoGrid').addClass('col-3')
-    $('.btnOne').removeClass('active')
-    $('.btnTwo').removeClass('active')
-    $('.btnThree').removeClass('active')
+    $('#photoGridRow>div.photoGrid').addClass(['col-sm-3', 'col-6'])
+    $('#photoGridRow>div.photoGrid').removeClass(['col-sm-4', 'col-sm-6', 'col-12'])
 };
