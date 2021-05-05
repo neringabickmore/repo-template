f5bcc13250b36bc6b0c7660bf4e62ec8ccdb372a
neringa <neringa.bickmore@gmail.com>
2021-05-05 21:58:48 +0100

    portfolio/views/added login-required

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 
 import cloudinary
@@ -22,8 +23,16 @@ def portfolio(request):
 
     return render(request, template, context)
 
-
+@login_required
 def upload_photo(request):
+
+    if not request.user.is_superuser:
+        sweetify.sweetalert(
+            request, title='error', icon='error',
+            text="This functionality is available to admin only.",
+            timer=2000)
+        return redirect(reverse('portfolio'))
+
     if request.method == 'POST':
         upload_form = PhotoForm(request.POST, request.FILES)
         if upload_form.is_valid():
