2b774555985656cc072d1dc595dacc8aecf4e6d0
neringa <neringa.bickmore@gmail.com>
2021-05-05 21:56:00 +0100

    Account auth/added login&logout feature this is only available for the superuser and not any other site users.

--- jb_portfolio/urls.py
+++ jb_portfolio/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('bio/', include('bio.urls')),
     path('contact/', include('contact.urls')),
     path('', include('portfolio.urls')),
