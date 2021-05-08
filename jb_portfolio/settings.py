5cb063d93d952c01511885a935d5c3c264546bf5
neringa <neringa.bickmore@gmail.com>
2021-05-08 18:17:36 +0100

    StaticFiles/Added missing settings

--- jb_portfolio/settings.py
+++ jb_portfolio/settings.py
@@ -194,6 +194,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
@@ -205,12 +206,14 @@ if 'USE_CLOUDINARY' in os.environ:
     # media files
     DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
     CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
-    
+
     CLOUDINARY_DOMAIN_NAME = os.environ.get('CLOUDINARY_DOMAIN_NAME')
     MEDIA_URL = f'https://{CLOUDINARY_DOMAIN_NAME}/'
 
     # static files
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     
